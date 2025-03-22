8d5b98faf0e98348b239f15d385936b09589bb0b
Balpier <108562940+Balpier@users.noreply.github.com>
2025-03-22 10:51:37 -0500

    Código completo del laboratorio numero 2 - Actividades

---
+++ MetodosGene/Bolsa.java
@@ -0,0 +1,26 @@
+package MetodosGene;
+import java.util.Iterator; 
+import java.util.ArrayList; 
+
+public class Bolsa <T> implements Iterable <T> {
+	private ArrayList <T> lista = new ArrayList <T> ();
+	private int tope;
+	
+	public Bolsa(int tope) {
+		super();
+		this.tope = tope;
+		
+	}
+	public void add(T objeto) {
+		if (lista.size() >= tope) {
+			lista.add(objeto);
+		}else {
+			throw new RuntimeException("no cabem mas");
+		}
+	}
+	public Iterator <T> iterator(){
+		return lista.iterator();
+	}
+
+
+}
