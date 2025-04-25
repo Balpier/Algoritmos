034b989fa0b38a19313f4a95c293956b98151e45
Balpier <108562940+Balpier@users.noreply.github.com>
2025-04-24 20:43:28 -0500

    Busqueda de elementos

---
+++ Lab5/src/Actividads/Ejercicios/Ejercicio1.java
@@ -0,0 +1,15 @@
+package Ejercicios;
+
+import java.util.List;
+
+public class Ejercicio1 {
+    public static <T> boolean buscarElemento(List<T> lista, T valor) {
+        return lista.contains(valor);
+    }
+
+    public static void main(String[] args) {
+        List<String> nombres = List.of("Ana", "Luis", "Carlos");
+        System.out.println(buscarElemento(nombres, "Luis")); 
+        System.out.println(buscarElemento(nombres, "Fernando")); 
+    }
+}
